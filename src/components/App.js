e07b71b269de300b6bc9936b5f85c6bbc7264026
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-03 10:08:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const App = () => {
+  const relatives = ["Uncle", "Aunt", "Cousin", "Grandparents", "Brother"]
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+          <h1>My Relatives List for Diwali 🎆</h1>
+      <ol key="relativeList">
+        {relatives.map((name, index) => (
+          <li key={`relativeListItem${index + 1}`}>{name}</li>
+        ))}
+      </ol>
     </div>
   )
 }
